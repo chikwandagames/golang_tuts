8623f6475b43b1b30e2a5b64f1a5b016eb3f1ffa
chikwandagames <chikwandagames@gmail.com>
2021-02-24 15:16:49 +0000

    strings, string literals...

--- main.go
+++ main.go
@@ -7,10 +7,22 @@ var n = 45
 
 // declares a variable z of type int, with a default ZERO value
 var z int
+var s = "Shaken, not stirred"
+
+// String literal
+var m = `Hey player`
 
 func main() {
 	x := 42
 
 	fmt.Println(x)
+	// Print variable type
+	fmt.Printf("%T \n", x)
+
+	fmt.Println(s)
+	// Print variable type
+	fmt.Printf("%T \n", s)
+
+	fmt.Println(m)
 
 }
