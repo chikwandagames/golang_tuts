365ef740ab7ed8029d7375db1b921b00e918ba45
chikwandagames <chikwandagames@gmail.com>
2021-02-24 14:48:56 +0000

    var key word

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+// var lets you decalare a global variable
+var n = 45
+
+// declares a variable z of type int, with a default ZERO value
+var z int
+
 func main() {
 	x := 42
 
 	fmt.Println(x)
+
 }
