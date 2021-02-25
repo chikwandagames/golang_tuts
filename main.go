c169391f6e782d833b2aff39610d55c82e097edf
chikwandagames <chikwandagames@gmail.com>
2021-02-25 17:03:13 +0000

    basic funtion definition

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+// we define functions with parameters
+// we call our function and pass in arguments
+// everythin in go is PASS BY VALUE
+
+func main() {
+	printName("wisdom")
+
+	s1 := foo("David")
+	fmt.Println(s1)
+}
+
+// func (r receiver) ideintifire(parameters) (return(s)) { ... }
+func printName(s string) {
+	fmt.Printf("Hello %v\n", s)
+}
+
+func foo(st string) string {
+	// StringPrint, prints to a string
+	return fmt.Sprint("Hello from ", st)
 }
