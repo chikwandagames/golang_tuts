d5870df93652ad4ee960b63155a1c48219e88f4c
chikwandagames <chikwandagames@gmail.com>
2021-02-25 13:18:46 +0000

    switch statement

--- main.go
+++ main.go
@@ -3,19 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	x := 0
+	// DONT use fall through
+	switch {
+	case false:
+		fmt.Println("Nope")
+	case (2 == 4):
+		fmt.Sprintln("Nope")
+	case (3 == 5):
+		fmt.Println("nope")
+		fallthrough
+	case (3 == 3):
+		fmt.Println("yes")
+		fallthrough
+	case (4 == 4):
+		fmt.Println("yes")
+	default:
+		fmt.Println("default")
+	}
 
-	for {
-		x++
-		if x > 10 {
-			break
-		}
-		if x%2 != 0 {
-			continue
-		}
-
-		fmt.Println(x)
-
+	n := "Bond"
+	switch n {
+	case "Wisdom":
+		fmt.Println("nope")
+	case "Bond", "trev": // multipe cases
+		fmt.Println("james")
+	default:
+		fmt.Println("default")
 	}
 
 }
