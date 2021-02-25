caef011c6a9abddec4f2506fa884c3432f8b70ed
chikwandagames <chikwandagames@gmail.com>
2021-02-25 12:15:06 +0000

    for statement

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
+	x := 1
+	// for statemaent with single condition
+	for x < 5 {
+		fmt.Println(x)
+		x++
+	}
+
+	fmt.Println("")
+	// this for is like a while loop
+	j := 1
+	for {
+		if j > 5 {
+			break
+		}
+		fmt.Println(j)
+		j++
+	}
+
+	fmt.Println("")
+	// for statement with a clause
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
 
 }
