1e3634cb90cf46b1885c610b28e84e603eb22cac
chikwandagames <chikwandagames@gmail.com>
2021-02-25 13:48:21 +0000

    basic struct definition

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
+// A struct is a composite data type of aggregate data type or complex data type
+// It allows us to compose together values of different types
+type person struct {
+	firstname string
+	lastname  string
+}
+
 func main() {
+	// P1 is a value of type person
+	p1 := person{
+		firstname: "Jame",
+		lastname:  "Bond",
+	}
+
+	p2 := person{
+		firstname: "Miss",
+		lastname:  "Moneypenny",
+	}
+
+	fmt.Println(p1)
+	fmt.Println(p2)
+	fmt.Println(p1.firstname)
 
 }
