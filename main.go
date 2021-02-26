58a6b5367476c4df4236f1afb8f54431de73d132
chikwandagames <chikwandagames@gmail.com>
2021-02-26 10:49:08 +0000

    slice destructuring

--- main.go
+++ main.go
@@ -4,14 +4,27 @@ import "fmt"
 
 func main() {
 
-	foo(1, 2, 3, 4, 5)
-	r := sum(1, 2, 3, 4, 5)
-	fmt.Printf("total = %v \n", r)
+	xi := []int{2, 3, 4, 5, 6, 7}
+
+	// Slice DESTRUCTURING
+	x := sum(xi...)
+	fmt.Println(x)
+	fmt.Println("")
+
+	foo(xi...)
+	foo(6, 7, 8, 9)
+	foo()
+
 }
 
+// You can pass zero or more ints to this function
+// The final parameter has to be 0 or more
+// foo(s string, x ...int)  GOOD
+// foo(x ...int, s string)  BAD
 func foo(x ...int) {
 	// x, will be stored as a slice of ints, []int
 	fmt.Println(x)
+	fmt.Printf("length: %v \n", len(x))
 	fmt.Printf("%T\n", x)
 }
 
