1151affeae5c4a1dac56d57e71752528eb48eb1a
chikwandagames <chikwandagames@gmail.com>
2021-02-26 16:44:07 +0000

    callbacks

--- main.go
+++ main.go
@@ -1,27 +1,57 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// CALLBACKS
+// Passing a function as an argument
 
 func main() {
-	x := bar()
-	fmt.Printf("%T \n", x)
 
-	i := x()
-	fmt.Println(i)
+	ii := []int{1, 2, 3, 4, 5}
+	s := sum(ii...)
+	fmt.Println(s)
 
-	// Alternatively
-	// Here we execute both bar() and its returned function
-	f := bar()()
-	fmt.Println(f)
+	s2 := even(sum, ii...)
+	fmt.Println("even numbers sum:", s2)
 
 }
 
-// bar(), returns a function that returns and int
-// functions are types in Golang
-func bar() func() int {
-	return func() int {
-		return 500
+func sum(xi ...int) int {
+
+	total := 0
+	for _, v := range xi {
+		total += v
 	}
+	return total
 }
+
+func even(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 0 {
+
+			yi = append(yi, v)
+		}
+	}
+	fmt.Println("even numbers:", yi)
+	// f is sum() in this case
+	return f(yi...)
+}
+
+// func filterEvens(xi ...int) []int {
+
+// 	ls := []int{}
+
+// 	for _, v := range xi {
+// 		if v%2 == 0 {
+// 			ls = append(ls, v)
+// 		}
+// 	}
+// 	return ls
+// }
+
+// func addEvens(f func(xi ...int) []int, vi ...int) int {
+// 	vl := f(vi...)
+// 	r := sum(vl...)
+// 	return r
+// }
