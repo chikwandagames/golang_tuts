b3482e0ded72c76f60ac3a8b1df07efe0f60da9d
chikwandagames <chikwandagames@gmail.com>
2021-02-26 18:49:00 +0000

    closures

--- main.go
+++ main.go
@@ -2,56 +2,38 @@ package main
 
 import "fmt"
 
-// CALLBACKS
-// Passing a function as an argument
+// CLOSURE
+// Closure is about enlosing the scope of a variable to make sure
+// its scope is limited
+
+/*
+	A closure is function value that references variables from outside its own
+  function body,
+	i.e. and inner function that remembers and has access to variables in the local
+	scope in which it was created even after the outer function is done executin
+*/
+
+// increment() returns a fuction that returns an int
+func increment() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
 
 func main() {
+	nextInt := increment()
+	// The value keep incrementing because the inner anonymous function is
+	// keeping track of the value i in its outer scope, even though increment()
+	// is done executing
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	print("\n")
 
-	ii := []int{1, 2, 3, 4, 5}
-	s := sum(ii...)
-	fmt.Println(s)
-
-	s2 := even(sum, ii...)
-	fmt.Println("even numbers sum:", s2)
-
+	nextInt2 := increment()
+	fmt.Println(nextInt2())
+	fmt.Println(nextInt2())
+	fmt.Println(nextInt2())
 }
-
-func sum(xi ...int) int {
-
-	total := 0
-	for _, v := range xi {
-		total += v
-	}
-	return total
-}
-
-func even(f func(xi ...int) int, vi ...int) int {
-	var yi []int
-	for _, v := range vi {
-		if v%2 == 0 {
-
-			yi = append(yi, v)
-		}
-	}
-	fmt.Println("even numbers:", yi)
-	// f is sum() in this case
-	return f(yi...)
-}
-
-// func filterEvens(xi ...int) []int {
-
-// 	ls := []int{}
-
-// 	for _, v := range xi {
-// 		if v%2 == 0 {
-// 			ls = append(ls, v)
-// 		}
-// 	}
-// 	return ls
-// }
-
-// func addEvens(f func(xi ...int) []int, vi ...int) int {
-// 	vl := f(vi...)
-// 	r := sum(vl...)
-// 	return r
-// }
