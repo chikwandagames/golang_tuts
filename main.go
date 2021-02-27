8c96d2cde6bc02dcb9f41a2ad6a22dd30504ea87
chikwandagames <chikwandagames@gmail.com>
2021-02-27 19:11:38 +0000

    goroutines, sequencial example

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+// GORUTINES
+// Goroutines are like very lightweight threads managed by the go runtime
+// they enable us to create asychronous parrallel programs
+
+// Go routines is a way to turn a sequential program into a concurrent program
+// without the need for threads
+
+func compute(value int) {
+	for i := 0; i < value; i++ {
+		time.Sleep(time.Second)
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	compute(5)
+	compute(5)
 }
