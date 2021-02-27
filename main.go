0d9b4dab1a7c6b510321340fc7569813418d3c46
chikwandagames <chikwandagames@gmail.com>
2021-02-27 12:09:04 +0000

    comma ok idiom

--- main.go
+++ main.go
@@ -14,4 +14,17 @@ func main() {
 	fmt.Println(rm)
 	fmt.Println(rm["england"])
 
+	// comma ok idiom, lets you check a field exists before accessing
+	v, ok := rm["zaire"]
+	fmt.Println(v)
+	fmt.Println(ok)
+
+	// we can check a value exists using an if statement
+	if v, ok := rm["zaire"]; ok {
+		fmt.Println(v)
+	}
+
+	if v, ok := rm["england"]; ok {
+		fmt.Println(v)
+	}
 }
