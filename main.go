aa122f52103014371c8693a06ba53d2628148498
chikwandagames <chikwandagames@gmail.com>
2021-02-27 10:23:44 +0000

    append and delete from slice

--- main.go
+++ main.go
@@ -8,16 +8,17 @@ func main() {
 	// A composit literal, is an expression that creates a new instance
 	// each time its evaluated,
 	// they can be user to create Arrays, Structs, Slices and maps
-	x := []int{1, 2, 3, 4, 5, 6}
+	x := []int{0, 1, 2, 3, 4, 5, 6}
 	fmt.Println(x)
 
-	// up to but not including
-	fmt.Println(x[:3])
+	x = append(x, 8, 9, 10)
+	fmt.Println(x)
 
-	// from position 2 to the end
-	fmt.Println(x[2:])
-
-	// from start to second last element
-	fmt.Println(x[:len(x)-1])
+	y := []int{11, 12, 13, 14, 15}
+	x = append(x, y...)
+	fmt.Println(x)
+	fmt.Println("deleting 4 and position 3 in the slice")
+	x = append(x[:4], x[5:]...)
+	fmt.Println(x)
 
 }
