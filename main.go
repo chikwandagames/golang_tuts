a401606208cead988abe09d13ce51cb48f545ead
chikwandagames <chikwandagames@gmail.com>
2021-03-01 17:50:13 +0000

    main branch update

--- main.go
+++ main.go
@@ -1,47 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"sync"
-)
-
-var wg sync.WaitGroup
-
 func main() {
-
-	fmt.Println("OS\t\t", runtime.GOOS)
-	fmt.Println("ARCH\t\t", runtime.GOARCH)
-	fmt.Println("CPUs\t\t", runtime.NumCPU())
-	fmt.Println("Goroutines\t", runtime.NumGoroutine())
-
-	// Because main() completes execution before the goroutine
-	// we use a waitgroup to await the execution of the goroutine to complete
-	// before exiting main
-
-	// 1. add one thing to wait for
-	wg.Add(1)
-	// launch a new go routine
-	go foo()
-	bar()
-
-	fmt.Println("CPUs\t\t", runtime.NumCPU())
-	fmt.Println("Goroutines\t", runtime.NumGoroutine())
-
-	// Stops the waiting
-	wg.Wait()
-}
-
-func foo() {
-	for i := 0; i < 5; i++ {
-		fmt.Println("foo:", i)
-	}
-	// 2. remove that thing we are waiting for
-	wg.Done()
-}
-
-func bar() {
-	for i := 0; i < 5; i++ {
-		fmt.Println("bar:", i)
-	}
 }
