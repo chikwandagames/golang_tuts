d9bd8a53dd04d6bbf91f8fcafdf4f6ad8c047ae3
chikwandagames <chikwandagames@gmail.com>
2021-04-07 14:46:04 +0100

    write to file

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 /*
 	type error interface {
@@ -11,25 +17,26 @@ import "fmt"
 // Any type that implements Error() string is also of type error
 
 func main() {
-	var answer1, answer2, answer3 string
-
-	fmt.Print("Name: ")
-	_, err := fmt.Scan(&answer1)
+	// Create new file, returns a pointer to an error
+	f, err := os.Create("names.txt")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
+	defer f.Close()
 
-	fmt.Print("Favourite food: ")
-	_, err = fmt.Scan(&answer2)
+	// Returns a Reader
+	r := strings.NewReader("Hello")
+
+	// Copy reader "string" into file
+	io.Copy(f, r)
+
+	// Now read from the file
+	// This wont work, because the read operation will run before the
+	// write operation has completed
+	bs, err := ioutil.ReadAll(f)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-
-	fmt.Print("Favourite sport: ")
-	_, err = fmt.Scan(&answer3)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(answer1, answer2, answer3)
+	fmt.Println(string(bs))
 }
