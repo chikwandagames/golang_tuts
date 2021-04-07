302357c7568e052471bc6f13e62ab996dd75780e
chikwandagames <chikwandagames@gmail.com>
2021-04-07 13:06:07 +0100

    basic example

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import "fmt"
+
+/*
+	type error interface {
+		Error() string
+	}
+*/
+
+// Any type that implements Error() string is also of type error
+
 func main() {
+	n, err := fmt.Println("hello")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Prints 6 (number of bytes) because of the new line character on printLN
+	fmt.Println(n)
 }
